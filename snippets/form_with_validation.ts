44342e703bab4b5173025eebf0d72e5b6a1a4b0a
Çağan Seyrek <115536535+caganseyrek@users.noreply.github.com>
2024-09-07 22:09:50 +0100

    Create form_with_validation.ts

---
+++ snippets/form_with_validation.ts
@@ -0,0 +1,64 @@
+import { Controller, FieldValues, useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z, ZodSchema } from "zod";
+
+const loginSchema: ZodSchema = z.object({
+  email: z
+    .string({ required_error: "This field is required" })
+    .min(1, "This field is required")
+    .email("Please enter a valid e-mail address"),
+  password: z.string({ required_error: "This field is required" }).min(1, "This field is required"),
+});
+
+type LoginFormData = z.infer<typeof loginSchema>;
+
+const form_with_validation = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const FormSubmitAction = async (formdata: FieldValues) => {
+    try {
+      // handle submit functions
+    } catch (error) {
+      // handle errors
+    }
+  };
+
+  return (
+    <form noValidate onSubmit={handleSubmit(FormSubmitAction)}>
+      <Controller
+        name="email"
+        control={control}
+        render={({ field }) => {
+          return (
+            /* add custom inputs */
+            <input {...field} />
+          );
+        }}
+      />
+      <Controller
+        name="password"
+        control={control}
+        render={({ field }) => {
+          return (
+            /* add custom inputs */
+            <input type="password" {...field} />
+          );
+        }}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default form_with_validation;
